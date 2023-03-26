c08e0cb24eaa060e42a2dae7839af0873f9cb3f0
ismail <ismail.durak@etu.univ-orleans.fr>
2023-03-26 21:33:09 +0200

    maj des class vu que je m étais rater lol

--- backend_quizz/database/migrations/2023_03_26_192749_create_recompenses_table.php
+++ backend_quizz/database/migrations/2023_03_26_192749_create_recompenses_table.php
@@ -11,7 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('recompenses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('recompenses');
     }
 };
